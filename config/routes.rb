429a23cfe0b002beb381b7bffd9b78acc3abceaf
Marisa Cassidy <marisancassidy@gmail.com>
2021-01-09 14:12:29 +0900

    Added CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read all tasks
   get '/tasks', to: 'tasks#index'
+  # read one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # create a task
+  get '/tasks/new', to: 'tasks#edit'
+  post '/tasks', to: 'tasks#create'
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_01_09_020539) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
