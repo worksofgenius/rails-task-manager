d10181df846c982309993b6209d67cbf3acf98fd
Marisa Cassidy <marisancassidy@gmail.com>
2021-01-09 17:38:19 +0900

    Added working edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    redirect_to task_path
+    redirect_to tasks_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read all tasks
   get '/tasks', to: 'tasks#index'
+  # create a task
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
   # read one task
   get '/tasks/:id', to: 'tasks#show', as: :task
-  # create a task
-  get '/tasks/new', to: 'tasks#edit'
-  post '/tasks', to: 'tasks#create'
   # update a task
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   # delete a task
   delete 'tasks/:id', to: 'tasks#destroy'
